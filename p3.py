2e4e735837a8c5f9b1d9e3e6181d6fe96d9e8d37
DixitAnjali01 <122504032+DixitAnjali01@users.noreply.github.com>
2023-02-03 23:02:16 +0530

    Add files via upload

---
+++ p3.py
@@ -0,0 +1,9 @@
+#PRINT CHARACTERS FROM STRING THAT ARE PRESENT AT EVEN INDEX NUMBER
+str=input('Enter the string : ')
+print('Original string is ',str)
+print("Printing even index numbers:")
+for ch in range(100):
+    if(ch%2==0):
+        print(str[ch])
+    
+
