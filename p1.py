014f1b1d8c29929f67e18e683e0223614868ec22
DixitAnjali01 <122504032+DixitAnjali01@users.noreply.github.com>
2023-02-03 23:01:02 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,7 @@
+#PROGRAM TO CALCULATE AND MULTIPLICATION OF TWO NUMBERS
+num1=int(input('Enter first number:'))
+num2=int(input('Enter second number:'))
+product=num1*num2
+sum=num1+num2
+print('The multiplicatipn of the two numbers is =',product)
+print('The multiplicatipn of the two numbers is =',sum)
